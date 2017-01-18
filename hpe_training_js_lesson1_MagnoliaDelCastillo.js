956caee05f66e419b1a40520644b0a0f88b3ab6c
Magnolia2023 <magnolia.del-castillo-juptier@hpe.com>
2017-01-18 11:57:41 -0600

    Commit 1
    First exercise

---
+++ hpe_training_js_lesson1_MagnoliaDelCastillo.js
@@ -0,0 +1,31 @@
+// Exercise 1-2 ***********************************************************
+console.log("Hi all");
+console.log("this is my first activity");
+console.log("I'm very exiting to learn something new").length;
+console.log("I always have been interting in learn java code").length;
+console.log("This is a great opportunity").length;
+console.log("Thank you very much Ricardo for your time");
+console.log("your effort and disposition to teach us");
+console.log("God bless you with a lot of kids jijiji");
+console.log("joking");
+console.log("see you!!");
+//Exercise 2-3 ****************************************************************
+var greeting="Hi all";
+greeting.length;
+"this is my first activity".length;
+var num=44;
+var nam="Magnolia";
+var lastname="del Castillo Juptier";
+var son="Jaasiel";
+// Exercise 4 *****************************************************************
+confirm ("are you 20 years old?");
+confirm ("is your name Javier?");
+confirm ("are you from Aguascalientes?");
+confirm ("are you working on site?");
+confirm ("are you in testing area?");
+// Exercise 5 ****************************************************************
+var namepets=["Principe","Princesa","Toto","Pelusa"];
+var colors=["red","purple","yellow"];
+var prices=[20.00,100.00,50.00,35.00];
+var size=["small","medium","large","extralarge"];
+var age=[1,2,3,4,5,6,7,8,9];
