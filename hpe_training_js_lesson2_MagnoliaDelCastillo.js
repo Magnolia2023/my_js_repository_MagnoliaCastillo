bbec0b56ee687e0390249015dbaf043af1a7bc36
Magnolia2023 <magnolia.del-castillo-juptier@hpe.com>
2017-02-09 09:26:49 -0600

    Lesson 2
    First revision

---
+++ hpe_training_js_lesson2_MagnoliaDelCastillo.js
@@ -0,0 +1,68 @@
+//User enter name and program verifica si es mayor o menor a 8 caracteres
+var myName=prompt ("Ingrese su nombre de almenos 8 caracteres");
+
+if( "myName".length >= 8 ) {
+21     console.log("True");
+22 }
+23 else {
+24     console.log("False");
+25 }
+
+//Returns True
+10<100
+"I'm coding like a champ".length > 10
+5<10
+"Today is Wednesday and it's sunny".length > 10
+//Returns False
+10<1
+"I'm coding like a champ".length < 10
+5>10
+"Today is Wednesday and it's sunny".length < 10
+//Substrings
+"Hello".substring(0, 2)
+"My name is:".substring(2, 3)
+"Magnolia".substring(0, 5)
+"del Castillo".substring(1, 3)
+"Juptier".substring(5, 7)
+//Arrays
+var favouriteactivities= ["Go to the gym","Swim","Play Guitar"];
+var name=favouriteactivities [1];
+console.log (name);
+var favouritepets= ["Dog","Cat","Uron"];
+var favouritecolours= ["Purple","Blue","Yellow"];
+var favouritedays= ["Friday","Saturday","Sunday"];
+var favouriteplaces= ["Gym","Park","Home","Church"];
+//If
+var lessonnumber = prompt("What lesson number did you finish?");
+If (lessonsnumber>=3){
+     console.log("I'm doing it in the right way");
+ }
+ else {
+     console.log("I'm not doing it in the right way");
+ }
+ If (6<7){
+     console.log("I'm doing it in the right way");
+ }
+ If (5<7){
+     console.log("I'm doing it in the right way");
+ }
+ If (4<7){
+     console.log("I'm doing it in the right way");
+ }
+ If (3<7){
+     console.log("I'm doing it in the right way");
+ }
+ If (2<7){
+     console.log("I'm doing it in the right way");
+ }
+ If (1<7){
+     console.log("I'm doing it in the right way");
+ }
+ //Module Operations returns 0
+ 36 % 6 === 0
+ 4 % 2 === 0
+ 12 % 4 === 0
+ // Module Operations returns 1
+ 123 % 4 === 0
+ 40 % 6 === 0
+ 321 % 9 === 0
